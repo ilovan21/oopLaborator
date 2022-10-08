029ddd49b06de95a6d3b72bb9f480cecc4c40a4b
ilovm2 <99871534+ilovm2@users.noreply.github.com>
2022-10-08 08:18:22 +0300

    Update Main.java

--- src/LaboratorOOP/Main.java
+++ src/LaboratorOOP/Main.java
@@ -2,10 +2,10 @@ package LaboratorOOP;
 
 public class Main {
     public static void main(String[] args){
-        Student FilieraFrancofona=new Student();
-        FilieraFrancofona.afisare();
-        Student FilieraAnglofona=new Student("FAF-211","Caterev Ana");
-        FilieraAnglofona.afisare();
+        Student filieraFrancofona=new Student();
+        filieraFrancofona.afisare();
+        Student filieraAnglofona=new Student("FAF-211","Caterev Ana");
+        filieraAnglofona.afisare();
 
         Student grupaRomana=new Student("MI-211","Boboc Andrei","Managementul Informatiei" ,3);
         grupaRomana.afisare();;
